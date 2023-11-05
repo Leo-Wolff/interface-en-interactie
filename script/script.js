83b81792b795dcaa78eb59ac432f8270c1321dfd
Leo-Wolff <leokramer17@gmail.com>
2023-11-05 11:43:13 +0100

    Finish first section styling + add comments

--- script/script.js
+++ script/script.js
@@ -1,30 +1,26 @@
-const testButton = document.querySelector(
-	"article > section:first-of-type button:last-of-type"
+// VARIABLES
+const navButtons = document.querySelectorAll(
+	"article > section:first-of-type section button"
 )
-const clueText = document.querySelectorAll(
-	"article > section:first-of-type li:first-of-type p"
-)
-let paragraphID = 0
-
-// console.log(paragraphID)
-
-// Function to handle drag start event
-function dragStart(event) {
-	event.dataTransfer.setData("text", event.target.id)
-}
-
-// Add this JavaScript code to your script.js file
-
-const prevBtn = document.getElementById("prevBtn")
-const nextBtn = document.getElementById("nextBtn")
 const liElements = document.querySelectorAll(
 	"article > section:first-of-type ul li"
 )
+const testButton = document.querySelector("article > section:first-of-type a")
+const firstLiClueText = document.querySelectorAll(
+	"article > section:first-of-type li:first-of-type p:not(:first-of-type)"
+)
+// let is used here so the variable can be changed throughout use of the website
+let paragraphID = 0
 let currentIndex = 0
+
+//
+// NAVIGATION
+//
 
 function showCurrentLi() {
 	liElements.forEach((li, index) => {
-		if (index === currentIndex) {
+		if (index == currentIndex) {
+			// add a class to the current li
 			li.classList.add("active")
 		} else {
 			li.classList.remove("active")
@@ -32,33 +28,41 @@ function showCurrentLi() {
 	})
 }
 
-prevBtn.addEventListener("click", () => {
-	currentIndex = (currentIndex - 1 + liElements.length) % liElements.length
+navButtons[0].addEventListener("click", () => {
+	// go to the previous li, if there is no more elements to go back to, wrap back around
+	currentIndex = (currentIndex - 1) % liElements.length
 	showCurrentLi()
 })
 
-nextBtn.addEventListener("click", () => {
+navButtons[1].addEventListener("click", () => {
+	// go to the next li, if there is no more elements to go forward to, wrap back around
 	currentIndex = (currentIndex + 1) % liElements.length
 	showCurrentLi()
 })
 
-// Initially, show the first li element
+// initiate the function upon load to load the first li
 showCurrentLi()
 
+//
+// CHANGE CLUE TEXT
+//
+
 testButton.addEventListener("click", () => {
+	// if the first li is being shown
 	if (currentIndex == 0) {
 		if (paragraphID == 0) {
-			clueText[0].textContent =
+			firstLiClueText[0].textContent =
 				"Dean and Sam Winchester were on a new case. Someone disappeared in a most curious Spot."
 
+			//if the first clue gets revealed, increase the paragraphID so that more text can be revealed upon the next button press
 			paragraphID++
 		} else if (paragraphID == 1) {
-			clueText[1].textContent =
+			firstLiClueText[1].textContent =
 				"They decide to investigate at night, but suddenly got interrupted."
 
 			paragraphID++
 		} else if (paragraphID == 2) {
-			clueText[2].textContent =
+			firstLiClueText[2].textContent =
 				"Dean dies and Sam is most upset. Until he wakes up, it is Tuesday again and Dean is alive."
 
 			document
@@ -70,6 +74,13 @@ testButton.addEventListener("click", () => {
 		} else {
 			return
 		}
-		// console.log(paragraphID)
 	}
 })
+
+//
+// DRAG AND DROP
+//
+
+function dragStart(event) {
+	event.dataTransfer.setData("text", event.target.id)
+}
