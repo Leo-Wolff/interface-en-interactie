bee01cfa8aeaa3548a983fee5699a2f861ee3607
Leo-Wolff <leokramer17@gmail.com>
2023-11-05 14:43:42 +0100

    Finish styling section 2 + drag and drop function

--- script/script.js
+++ script/script.js
@@ -1,21 +1,15 @@
-// VARIABLES
+//
+// NAVIGATION
+//
+
 const navButtons = document.querySelectorAll(
 	"article > section:first-of-type section button"
 )
 const liElements = document.querySelectorAll(
 	"article > section:first-of-type ul li"
 )
-const testButton = document.querySelector("article > section:first-of-type a")
-const firstLiClueText = document.querySelectorAll(
-	"article > section:first-of-type li:first-of-type p:not(:first-of-type)"
-)
 // let is used here so the variable can be changed throughout use of the website
-let paragraphID = 0
 let currentIndex = 0
-
-//
-// NAVIGATION
-//
 
 function showCurrentLi() {
 	liElements.forEach((li, index) => {
@@ -44,43 +38,65 @@ navButtons[1].addEventListener("click", () => {
 showCurrentLi()
 
 //
-// CHANGE CLUE TEXT
-//
-
-testButton.addEventListener("click", () => {
-	// if the first li is being shown
-	if (currentIndex == 0) {
-		if (paragraphID == 0) {
-			firstLiClueText[0].textContent =
-				"Dean and Sam Winchester were on a new case. Someone disappeared in a most curious Spot."
-
-			//if the first clue gets revealed, increase the paragraphID so that more text can be revealed upon the next button press
-			paragraphID++
-		} else if (paragraphID == 1) {
-			firstLiClueText[1].textContent =
-				"They decide to investigate at night, but suddenly got interrupted."
-
-			paragraphID++
-		} else if (paragraphID == 2) {
-			firstLiClueText[2].textContent =
-				"Dean dies and Sam is most upset. Until he wakes up, it is Tuesday again and Dean is alive."
-
-			document
-				.querySelector("article > section:first-of-type li:first-of-type img")
-				.classList.add("hidden")
-			document
-				.querySelector("article > section:first-of-type li:first-of-type video")
-				.classList.remove("hidden")
-		} else {
-			return
-		}
-	}
-})
-
-//
 // DRAG AND DROP
 //
+const targetImage = document.querySelector(
+	"article > section:first-of-type li:first-of-type img"
+)
+
+const tableImages = document.querySelectorAll(
+	"article > section:nth-of-type(2) img"
+)
+let draggedImage
 
 function dragStart(event) {
-	event.dataTransfer.setData("text", event.target.id)
+	draggedImage = event.target
+}
+
+function allowDrop(event) {
+	event.preventDefault()
+}
+
+//
+// CHANGE CLUE TEXT
+//
+const firstLiClueText = document.querySelectorAll(
+	"article > section:first-of-type li:first-of-type p:not(:first-of-type)"
+)
+let paragraphID = 0
+
+function drop(event) {
+	event.preventDefault()
+
+	if (event.target == targetImage) {
+		if (currentIndex == 0) {
+			if (paragraphID == 0 && draggedImage == tableImages[0]) {
+				firstLiClueText[0].textContent =
+					"Dean and Sam Winchester were on a new case. Someone disappeared in a most curious Spot."
+
+				//if the first clue gets revealed, increase the paragraphID so that more text can be revealed upon the next button press
+				paragraphID++
+				draggedImage.classList.add("hidden")
+			} else if (paragraphID == 1) {
+				firstLiClueText[1].textContent =
+					"They decide to investigate at night, but suddenly got interrupted."
+
+				paragraphID++
+			} else if (paragraphID == 2) {
+				firstLiClueText[2].textContent =
+					"Dean dies and Sam is most upset. Until he wakes up, it is Tuesday again and Dean is alive."
+
+				document
+					.querySelector("article > section:first-of-type li:first-of-type img")
+					.classList.add("hidden")
+				document
+					.querySelector(
+						"article > section:first-of-type li:first-of-type video"
+					)
+					.classList.remove("hidden")
+			} else {
+				return
+			}
+		}
+	}
 }
