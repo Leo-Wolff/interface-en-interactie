09b687ded52b358d1d93cfa301e4a7686ffe6d32
Leo-Wolff <leokramer17@gmail.com>
2023-11-07 22:23:59 +0100

    Add all levels + easter egg

--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
 //
+//
+//
+//
 // TUTORIAL
 //
 document
@@ -11,10 +14,28 @@ document
 			.classList.add("hidden")
 	})
 
+const elementsToTrack = document.querySelectorAll(
+	"input, button, a, [tabindex], img"
+)
+
+for (const element of elementsToTrack) {
+	element.addEventListener("focus", () => {
+		console.log("Focused element:", element)
+	})
+}
+
+document.addEventListener("keydown", (e) => {
+	if (e.keyCode === 9) {
+		const currentlyFocusedElement = document.activeElement
+		console.log("Active element:", currentlyFocusedElement)
+	}
+})
+//
+//
+//
 //
 // NAVIGATION
 //
-
 const navButtons = document.querySelectorAll(
 	"article > section:nth-of-type(2) section button"
 )
@@ -24,9 +45,10 @@ const liElements = document.querySelectorAll(
 // let is used here so the variable can be changed throughout use of the website
 let currentIndex = 0
 
+// Only when the current li is showing (the index being equal to currentindex), give it the class active
 function showCurrentLi() {
 	liElements.forEach((li, index) => {
-		if (index == currentIndex) {
+		if (index == currentIndex && !li.classList.contains("hidden")) {
 			// add a class to the current li
 			li.classList.add("active")
 		} else {
@@ -36,16 +58,16 @@ function showCurrentLi() {
 }
 
 navButtons[0].addEventListener("click", () => {
-	// go to the previous li, if there is no more elements to go back to, wrap back around
-	currentIndex = (currentIndex - 1) % liElements.length
+	// example math to understand the logic:
+	// currentindex = 2, liElements.length = 3:
+	// 2 - 1 + 3 = 4
+	// 4 %3 = 1
+	currentIndex = (currentIndex - 1 + liElements.length) % liElements.length
 
 	tableImages.forEach((img) => {
 		img.classList.remove("hidden")
 	})
 	shelfImages.forEach((img) => {
-		img.classList.remove("hidden")
-	})
-	trunkImages.forEach((img) => {
 		img.classList.remove("hidden")
 	})
 
@@ -59,8 +81,10 @@ navButtons[1].addEventListener("click", () => {
 	tableImages.forEach((img) => {
 		img.classList.remove("hidden")
 	})
-	shelfImages.forEach((img) => {
-		img.classList.remove("hidden")
+	shelfImages.forEach((img, index) => {
+		if (index !== 7) {
+			img.classList.remove("hidden")
+		}
 	})
 
 	showCurrentLi()
@@ -68,7 +92,9 @@ navButtons[1].addEventListener("click", () => {
 
 // initiate the function upon load to load the first li
 showCurrentLi()
-
+//
+//
+//
 //
 // DRAG AND DROP
 //
@@ -90,9 +116,11 @@ function dragStart(event) {
 function allowDrop(event) {
 	event.preventDefault()
 }
-
 //
-// CHANGE CLUE TEXT
+//
+//
+//
+// DATABASE
 //
 // This object has all of the different information that is needed for each specific item in the timeline
 const clueData = {
@@ -120,7 +148,73 @@ const clueData = {
 			"The other hunters are angry at Sam and Dean for starting the Apocalypse. They take matters into their own hands.",
 		],
 	},
+	2: {
+		firstImage: tableImages[3],
+		secondImage: shelfImages[2],
+		thirdImage: tableImages[1],
+		clueParagraph: document.querySelectorAll(
+			"article > section:nth-of-type(2) li:nth-of-type(3) p:not(:first-of-type)"
+		),
+		text: [
+			"With the First Blade, Dean hopes to be able to kill the enemy.",
+			"Dean dies at the hand of Metatron, but something unexpected happens.",
+		],
+	},
+	3: {
+		firstImage: shelfImages[0],
+		secondImage: shelfImages[4],
+		thirdImage: tableImages[2],
+		clueParagraph: document.querySelectorAll(
+			"article > section:nth-of-type(2) li:nth-of-type(4) p:not(:first-of-type)"
+		),
+		text: [
+			"Dean decides he needs to try to talk to someone about Sam's death.",
+			"To talk to Billie, also known as Death, Dean needs to die.",
+		],
+	},
+	4: {
+		firstImage: shelfImages[0],
+		secondImage: shelfImages[3],
+		thirdImage: shelfImages[4],
+		clueParagraph: document.querySelectorAll(
+			"article > section:nth-of-type(2) li:nth-of-type(5) p:not(:first-of-type)"
+		),
+		text: [
+			"Dean and Sam have no way of escape, they need to take drastic measures.",
+			"To escape the facility Dean makes a deal to have the brothers die and be brought back one more time, but at midnight one will be dying permanently.",
+		],
+	},
+	5: {
+		firstImage: tableImages[2],
+		secondImage: shelfImages[5],
+		thirdImage: shelfImages[4],
+		clueParagraph: document.querySelectorAll(
+			"article > section:nth-of-type(2) li:nth-of-type(6) p:not(:first-of-type)"
+		),
+		text: [
+			"Dean grabs the syringes, Sam protests against this idea. What do the syringes do?",
+			"Sam tries to start Dean's heart again with the other syringe, but someone else wishes to talk to Dean.",
+		],
+	},
+	6: {
+		firstImage: shelfImages[1],
+		secondImage: shelfImages[2],
+		thirdImage: shelfImages[4],
+		clueParagraph: document.querySelectorAll(
+			"article > section:nth-of-type(2) li:nth-of-type(7) p:not(:first-of-type)"
+		),
+		text: [
+			"Dean gets pushed against a certain object. What does it look similar to?",
+			"It's the last season. It is final.",
+		],
+	},
 }
+//
+//
+//
+//
+// CHANGE CLUE TEXT
+//
 let paragraphID = 0
 
 console.log(tableImages)
@@ -129,12 +223,21 @@ console.log(shelfImages)
 function drop(event) {
 	event.preventDefault()
 
+	if (draggedImage == shelfImages[7]) {
+		document
+			.querySelector("article > section:first-of-type > section:nth-of-type(2)")
+			.classList.remove("hidden")
+		liElements[7].classList.remove("hidden")
+	}
+
 	if (Array.from(targetImage).includes(event.target)) {
 		revealClues(event)
 	} else {
 		return
 	}
 }
+
+console.log(liElements)
 
 function revealClues(event) {
 	let currentClue = clueData[currentIndex]
@@ -162,6 +265,34 @@ function revealClues(event) {
 		}, 1000)
 
 		paragraphID++
+	} else if (
+		currentIndex == 5 &&
+		paragraphID == 2 &&
+		draggedImage == currentClue.thirdImage
+	) {
+		// Else if is used her for the easter egg, it is before the other else if statement because to override it.
+		targetImage[currentIndex].classList.add("spin")
+		draggedImage.classList.add("hidden")
+
+		setTimeout(function () {
+			targetImage[currentIndex].classList.add("disappear")
+		}, 1000)
+
+		setTimeout(function () {
+			liElements[currentIndex].querySelector("video").classList.remove("hidden")
+			liElements[currentIndex].querySelector("video").classList.add("appear")
+		}, 2000)
+
+		setTimeout(function () {
+			targetImage[currentIndex].classList.remove("spin", "disappear")
+			targetImage[currentIndex].classList.add("hidden")
+			liElements[currentIndex].querySelector("video").classList.remove("appear")
+
+			// EASTER EGG
+			shelfImages[7].classList.remove("hidden")
+		}, 3000)
+
+		paragraphID = 0
 	} else if (paragraphID == 2 && draggedImage == currentClue.thirdImage) {
 		targetImage[currentIndex].classList.add("spin")
 		draggedImage.classList.add("hidden")
