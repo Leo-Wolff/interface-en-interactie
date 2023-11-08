312d9b10b4a7503ba772d829b15001c8db3fff8b
Leo-Wolff <leokramer17@gmail.com>
2023-11-08 12:44:17 +0100

    Refactoring

--- script/script.js
+++ script/script.js
@@ -1,9 +1,8 @@
-//
-//
-//
-//
+// /////////////
 // TUTORIAL
-//
+// /////////////
+
+// Click the button to hide the zero state tutorial pop-up
 document
 	.querySelector(
 		"article > section:first-of-type > section:first-of-type button"
@@ -14,32 +13,15 @@ document
 			.classList.add("hidden")
 	})
 
-const elementsToTrack = document.querySelectorAll(
-	"input, button, a, [tabindex], img"
-)
-
-for (const element of elementsToTrack) {
-	element.addEventListener("focus", () => {
-		console.log("Focused element:", element)
-	})
-}
-
-document.addEventListener("keydown", (e) => {
-	if (e.keyCode === 9) {
-		const currentlyFocusedElement = document.activeElement
-		console.log("Active element:", currentlyFocusedElement)
-	}
-})
-//
-//
-//
-//
+// /////////////
 // NAVIGATION
-//
+// /////////////
+
+// VARIABLES
 const navButtons = document.querySelectorAll(
 	"article > section:nth-of-type(2) section button"
 )
-const liElements = document.querySelectorAll(
+const timelineLi = document.querySelectorAll(
 	"article > section:nth-of-type(2) ul li"
 )
 // let is used here so the variable can be changed throughout use of the website
@@ -47,9 +29,8 @@ let currentIndex = 0
 
 // Only when the current li is showing (the index being equal to currentindex), give it the class active
 function showCurrentLi() {
-	liElements.forEach((li, index) => {
+	timelineLi.forEach((li, index) => {
 		if (index == currentIndex && !li.classList.contains("hidden")) {
-			// add a class to the current li
 			li.classList.add("active")
 		} else {
 			li.classList.remove("active")
@@ -59,10 +40,10 @@ function showCurrentLi() {
 
 navButtons[0].addEventListener("click", () => {
 	// example math to understand the logic:
-	// currentindex = 2, liElements.length = 3:
+	// currentindex = 2, timelineLi.length = 3:
 	// 2 - 1 + 3 = 4
-	// 4 %3 = 1
-	currentIndex = (currentIndex - 1 + liElements.length) % liElements.length
+	// 4 % 3 = 1, because 1 is remaining
+	currentIndex = (currentIndex - 1 + timelineLi.length) % timelineLi.length
 
 	tableImages.forEach((img) => {
 		img.classList.remove("hidden")
@@ -75,8 +56,7 @@ navButtons[0].addEventListener("click", () => {
 })
 
 navButtons[1].addEventListener("click", () => {
-	// go to the next li, if there is no more elements to go forward to, wrap back around
-	currentIndex = (currentIndex + 1) % liElements.length
+	currentIndex = (currentIndex + 1) % timelineLi.length
 
 	tableImages.forEach((img) => {
 		img.classList.remove("hidden")
@@ -92,12 +72,12 @@ navButtons[1].addEventListener("click", () => {
 
 // initiate the function upon load to load the first li
 showCurrentLi()
-//
-//
-//
-//
+
+// /////////////
 // DRAG AND DROP
-//
+// /////////////
+
+// VARIABLES
 const targetImage = document.querySelectorAll(
 	"article > section:nth-of-type(2) li img"
 )
@@ -113,16 +93,16 @@ function dragStart(event) {
 	draggedImage = event.target
 }
 
+// Allow an item to be dropped at a certain place
+// (gets specified at change clue text section of the code)
 function allowDrop(event) {
 	event.preventDefault()
 }
-//
-//
-//
-//
+
+// /////////////
 // DATABASE
-//
-// This object has all of the different information that is needed for each specific item in the timeline
+// /////////////
+
 const clueData = {
 	0: {
 		firstImage: tableImages[1],
@@ -209,20 +189,17 @@ const clueData = {
 		],
 	},
 }
-//
-//
-//
-//
-// CHANGE CLUE TEXT
-//
-let paragraphID = 0
 
-console.log(tableImages)
-console.log(shelfImages)
+// /////////////
+// CHANGE CLUE TEXT
+// /////////////
+
+let paragraphIndex = 0
 
 function drop(event) {
 	event.preventDefault()
 
+	// If the mystery spot cube gets sacrificed, it unlocks the easter egg
 	if (draggedImage == shelfImages[7]) {
 		document
 			.querySelector("article > section:first-of-type > section:nth-of-type(2)")
@@ -230,6 +207,7 @@ function drop(event) {
 		liElements[7].classList.remove("hidden")
 	}
 
+	// If any target image is the same as the event target, then it can reveal a clue
 	if (Array.from(targetImage).includes(event.target)) {
 		revealClues(event)
 	} else {
@@ -237,83 +215,77 @@ function drop(event) {
 	}
 }
 
-console.log(liElements)
-
 function revealClues(event) {
+	// For better comprehension of the code
 	let currentClue = clueData[currentIndex]
 
-	if (paragraphID == 0 && draggedImage == currentClue.firstImage) {
+	if (paragraphIndex == 0 && draggedImage == currentClue.firstImage) {
 		currentClue.clueParagraph[1].textContent = currentClue.text[0]
 
+		// Spin animation when sacrifice is succesful
 		targetImage[currentIndex].classList.add("spin")
 		draggedImage.classList.add("hidden")
 
-		setTimeout(function () {
+		// Stop animation when finished
+		setTimeout(() => {
 			targetImage[currentIndex].classList.remove("spin")
 		}, 1000)
 
-		//if the first clue gets revealed, increase the paragraphID so that more text can be revealed upon the next button press
-		paragraphID++
-	} else if (paragraphID == 1 && draggedImage == currentClue.secondImage) {
+		// If the first clue gets revealed,
+		// increase the paragraphIndex so that more text can be
+		// revealed upon the next button press
+		paragraphIndex++
+	} else if (paragraphIndex == 1 && draggedImage == currentClue.secondImage) {
 		currentClue.clueParagraph[2].textContent = currentClue.text[1]
 
 		targetImage[currentIndex].classList.add("spin")
 		draggedImage.classList.add("hidden")
 
-		setTimeout(function () {
+		setTimeout(() => {
 			targetImage[currentIndex].classList.remove("spin")
 		}, 1000)
 
-		paragraphID++
-	} else if (
-		currentIndex == 5 &&
-		paragraphID == 2 &&
-		draggedImage == currentClue.thirdImage
-	) {
-		// Else if is used her for the easter egg, it is before the other else if statement because to override it.
+		paragraphIndex++
+	} else if (paragraphIndex == 2 && draggedImage == currentClue.thirdImage) {
 		targetImage[currentIndex].classList.add("spin")
 		draggedImage.classList.add("hidden")
 
-		setTimeout(function () {
-			targetImage[currentIndex].classList.add("disappear")
-		}, 1000)
+		finalAnimation()
 
-		setTimeout(function () {
-			liElements[currentIndex].querySelector("video").classList.remove("hidden")
-			liElements[currentIndex].querySelector("video").classList.add("appear")
-		}, 2000)
-
-		setTimeout(function () {
-			targetImage[currentIndex].classList.remove("spin", "disappear")
-			targetImage[currentIndex].classList.add("hidden")
-			liElements[currentIndex].querySelector("video").classList.remove("appear")
-
-			// EASTER EGG
-			shelfImages[7].classList.remove("hidden")
-		}, 3000)
-
-		paragraphID = 0
-	} else if (paragraphID == 2 && draggedImage == currentClue.thirdImage) {
-		targetImage[currentIndex].classList.add("spin")
-		draggedImage.classList.add("hidden")
-
-		setTimeout(function () {
-			targetImage[currentIndex].classList.add("disappear")
-		}, 1000)
-
-		setTimeout(function () {
-			liElements[currentIndex].querySelector("video").classList.remove("hidden")
-			liElements[currentIndex].querySelector("video").classList.add("appear")
-		}, 2000)
-
-		setTimeout(function () {
-			targetImage[currentIndex].classList.remove("spin", "disappear")
-			targetImage[currentIndex].classList.add("hidden")
-			liElements[currentIndex].querySelector("video").classList.remove("appear")
-		}, 3000)
-
-		paragraphID = 0
+		paragraphIndex = 0
 	} else {
 		return
+	}
+}
+
+// /////////////
+// ANIMATION
+// /////////////
+
+function finalAnimation() {
+	setTimeout(() => {
+		targetImage[currentIndex].classList.add("disappear")
+	}, 1000)
+
+	setTimeout(() => {
+		timelineLi[currentIndex].querySelector("video").classList.remove("hidden")
+		timelineLi[currentIndex].querySelector("video").classList.add("appear")
+	}, 2000)
+
+	setTimeout(() => {
+		targetImage[currentIndex].classList.remove("spin", "disappear")
+		targetImage[currentIndex].classList.add("hidden")
+		timelineLi[currentIndex].querySelector("video").classList.remove("appear")
+	}, 3000)
+
+	// Easter egg
+	if (
+		currentIndex == 5 &&
+		paragraphIndex == 2 &&
+		draggedImage == currentClue.thirdImage
+	) {
+		setTimeout(() => {
+			shelfImages[7].classList.remove("hidden")
+		}, 3000)
 	}
 }
